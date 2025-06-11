8e7e7d5bf2a0aa8d55c2d4b5e72ec56b968eb052
fana00 <master@fana.club>
2025-06-11 15:14:05 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://7cbe5063-b845-44d8-a79d-37f07db28edd.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+          pattern: "/influencer/[^/]+",
+          metaDataEndpoint: "https://pktcxwwyadqahlmajzfh.supabase.co/functions/v1/influencer_metatdata/{slug}"
       }
-      // Add more patterns and their metadata endpoints as needed
   ]
 };
